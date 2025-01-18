ee8461096ac7dea328633cbd81705b495e7d28b3
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-01-18 22:24:38 +0500

    задание 4 урока 2

--- Task_02_04/Program.cs
+++ Task_02_04/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Month: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Day: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            DateTime birth = new DateTime(year, month, day);
+            DateTime now = DateTime.Now;
+
+            int age = now.Year - birth.Year;
+
+            if (now.Month < birth.Month || now.Month == birth.Month && now.Day < birth.Day)
+            {
+                age--;
+            }
+            if (age >= 18)
+            {
+                Console.WriteLine("Nice");
+            }
+            else
+            { 
+                Console.WriteLine("Bad");
+            }
         }
     }
 }
