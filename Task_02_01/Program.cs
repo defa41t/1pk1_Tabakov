a9890b77dd4844d0d52364ce2aaf20b72a074d92
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-01-18 22:01:17 +0500

    задание 1 урока 2

---
+++ Task_02_01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_02_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Math.PI;
+            double b = 6;
+            double c = 2;
+
+            double part1 = 5 * Math.Atan(a);
+            double part2 = 0.25 * Math.Cos(a);
+            double part3 = a + 3 * Math.Abs(a - b) + a * a;
+            double part4 = Math.Abs(a - b) * c + Math.Pow(a, 2);
+
+            double result = part1 - part2 * (part3 / part4);
+            Console.WriteLine(Math.Round(result, 2));
+
+        }
+    }
+}
