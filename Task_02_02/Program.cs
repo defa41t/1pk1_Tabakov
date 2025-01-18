5fae0c61daa9e46722686ed66116a710924b4450
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-01-18 22:14:42 +0500

    задание 2 урока 2

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 8;
+            double b = 14;
+            double c = Math.PI / 4;
+
+            double part1 = Math.Pow(a - 1, 1.0 / 3.0);
+            double part2 = Math.Pow(b + part1, 1.0 / 4.0);
+            double part3 = Math.Abs(a - b);
+            double part4 = Math.Pow(Math.Sin(c), 2);
+            double part5 = Math.Tan(c);
+
+            double result = part2 / (part3 * (part4 + part5));   
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Task_02_04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_02_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
