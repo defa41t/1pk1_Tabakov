1961825a678588f645465bb5224d3c59ea866c71
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-01-20 21:21:39 +0500

    задание 6 урока 3

---
+++ Task_03_06/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_03_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double h = 0.5;
+
+            for (double x = -4; x <= 4; x += h)
+            {
+                double y = Math.Abs(x);
+                Console.WriteLine($"x = {x}, y = {y}");
+            }
+        }
+    }
+}
