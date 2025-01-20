29f1c99800c208a050f50ee0e83bfb0f2caaeaf8
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-01-20 21:32:09 +0500

    задание 7 урока 3

---
+++ Task_03_07/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_03_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double g = 9.8;
+            double t = 0; 
+
+            while (t <= 10)
+            {
+                double v = g * t;
+                Console.WriteLine("t = " + t + "c, v = " + Math.Round(v, 1) + "м/c");
+                t += 0.5;
+            }
+        }
+    }
+}
