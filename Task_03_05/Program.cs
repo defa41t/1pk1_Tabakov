226f1bb7dcfc6b0ff187dd8aae18eaaf871c5a8c
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-01-20 21:12:44 +0500

    задание 5 урока 3

---
+++ Task_03_05/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_03_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начальную температуру в градусах Цельсия:");
+            double start = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конечную температуру в градусах Цельсия:");
+            double end = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите шаг изменения температуры:");
+            double step = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Температура в градусах Цельсия\tТемпература в градусах Фаренгейта");
+
+            for (double celsius = start; celsius <= end; celsius += step)
+            {
+                double fahrenheit = celsius * 1.8 + 32;
+                Console.WriteLine($"{celsius}\t\t\t\t{fahrenheit}");
+            }
+        }
+
+    }
+}
