34056e18160d67de7e42d9edea45e3c0f64d06d1
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-01-20 21:05:14 +0500

    задание 4 урока 3

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            while (true) 
+            {
+            Console.Write("Ввести данные: ");
+            string text = Console.ReadLine();
+            Console.Clear();
+                
+                if (text == "exit" ||  text == "")
+                break;
+                count++;
+            }
+            Console.WriteLine("Количество строк: " + count);
+        }
+    }
+}
