dd8dd169695a2e1a6547b0aa2e986d47f0c90e1d
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-01-25 18:31:24 +0500

    задание 5 урока 4

--- Task_03_05/Program.cs
+++ Task_03_05/Program.cs
@@ -2,8 +2,14 @@
 {
     internal class Program
     {
+        /* Написать программу, которая выводит на экран таблицу соответствия температуры в градусах Цельсия и 
+        Фаренгейта (F = C*1,8 + 32). Диапазон изменения температуры в градусах Цельсия и шаг должны вводиться во 
+        время работы программы
+
+         */
         static void Main(string[] args)
         {
+            //Ввод данных
             Console.WriteLine("Введите начальную температуру в градусах Цельсия:");
             double start = double.Parse(Console.ReadLine());
 
@@ -15,6 +21,7 @@
 
             Console.WriteLine("Температура в градусах Цельсия\tТемпература в градусах Фаренгейта");
 
+            //Расчеты
             for (double celsius = start; celsius <= end; celsius += step)
             {
                 double fahrenheit = celsius * 1.8 + 32;
--- Task_03_08/Program.cs
+++ Task_03_08/Program.cs
@@ -2,6 +2,8 @@
 {
     internal class Program
     {
+        /* Даны натуральные числа от 20 до 50. Напечатать те из них, которые делятся на 3, но не делятся на 5.
+         */
         static void Main(string[] args)
         {
             for (int i = 20; i < 50; i++)
