46265d6b5ede01eeee6f11f0f92fd943e3e147e5
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-01-27 10:34:57 +0500

    задание 8 урока 3

--- Task_03_08/Program.cs
+++ Task_03_08/Program.cs
@@ -10,7 +10,7 @@
             for (int i = 20; i < 50; i++)
             {
                 if (i % 3 == 0 && i % 5 != 0)
-                    Console.WriteLine(i);
+                    Console.Write(i + " ");
                 
 
             }
