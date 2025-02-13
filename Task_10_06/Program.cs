764417eba8c736533d961b1c81fe170b82f12d57
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-02-13 23:12:45 +0500

    Задание 6 урока 10

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_10_06
+{
+    internal class Program
+    {
+        /*Создать Метод ArrayGeneration не возвращающий значения, 
+         * принимает целое число n, 
+         * выводит наконсольсгенерированный массив размерности n*n.
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива n: ");
+            int n = int.Parse(Console.ReadLine());
+            // Вызов метода
+            ArrayGeneration(n);
+        }
+
+        static void ArrayGeneration(int n)
+        {
+            // Создаем двумерный массив
+            int[,] array = new int[n, n];
+
+            // Заполняем массив случайными числами
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = rand.Next(0, 101); 
+                }
+            }
+
+            // Вывод массива
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + " "); 
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
