1c5c9c645dc5ee6254115b7cca6656d050aff481
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-02-17 13:14:14 +0500

    Задание 2 урока 11

--- Task_11_02/Program.cs
+++ Task_11_02/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        /*Передача по ссылке (ref): Напишите метод, который принимает два целых числа по ссылке и
+        меняет их местами. Проверьте, изменились ли значения переменных вне метода
+         */
         static void Main(string[] args)
         {
-            
+            int num1 = 2;
+            int num2 = 7;
+            Console.WriteLine($"Числа: {num1}, {num2}");
+            Reverse(ref num1, ref num2);
+            Console.WriteLine($"Числа после замены: {num1}, {num2}");
+        }
+        static void Reverse(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
         }
     }
 }
