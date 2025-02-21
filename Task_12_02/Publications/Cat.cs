fe31bb505b02a3f3bad0261fe8ca31bf09e6843b
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-02-21 10:13:28 +0500

    Задание 2 урока 12

--- Task_12_02/Publications/Cat.cs
+++ Task_12_02/Publications/Cat.cs
@@ -11,5 +11,32 @@ namespace Task_12_02.Publications
         public string breed; // порода
         public string name; // кличка
         public string age; // возраст
+        public string color; // цвет
+        bool isMother = true; // проверка на наличие котят
+        bool childFree = true; // проверка на наличие котят
+
+        // метод класса - выдает информацию о кошке
+        public void CatInfo()
+        {
+            Console.WriteLine($"\n\nКошка под кличкой {name}: ее порода - {breed}, ее шерсть имеет {color} цвет, и ей {age}");
+        }
+
+        // метод класса - проверяет есть ли у кошки котята
+        public void IsMother()
+        {
+            if (isMother)
+                Console.WriteLine($"У кошки {name} есть котята");
+            else
+                Console.WriteLine("У кошки нет котят");
+        }
+
+        // метод класса - проверяет нет ли у кошки котят
+        public void IsChildFree()
+        {
+            if (childFree)
+                Console.WriteLine($"У кошки {name} нет котят");
+            else
+                Console.WriteLine("У кошки есть котята");
+        }
     }
 }
--- Task_12_02/Publications/Dog.cs
+++ Task_12_02/Publications/Dog.cs
@@ -8,5 +8,36 @@ namespace Task_12_02.Publications
 {
     internal class Dog
     {
+        public string breed; // порода
+        public string name; // кличка
+        public string age; // возраст
+        public string color; // цвет
+        bool isVaccine = true; // проверяет есть ли у собаки прививки
+        bool isNotVaccine = true;
+
+
+        // метод класса - выдает информацию о собаке
+        public void DogInfo()
+        {
+            Console.WriteLine($"\n\nСобаке под кличкой {name} {age}, ее порода {breed}, а ее шерстка имеет {color} цвет");
+        }
+
+        // метод класса - проверяет наличие прививок
+        public void IsVaccine()
+        {
+            if (isVaccine)
+                Console.WriteLine($"Собака {name} имеет все прививки");
+            else 
+                Console.WriteLine($"У собаки {name} нет прививок");
+        }
+
+        // метод класса - проверяет отсутствие прививок
+       public void IsNotVaccine()
+        {
+            if (isNotVaccine)
+                Console.WriteLine($"У собаки {name} нет прививок");
+            else 
+                Console.WriteLine("У собаки есть все прививки");
+        }
     }
 }
