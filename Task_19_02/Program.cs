78b84ddf388b350d90083392ebe58ea6af215563
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-03-24 20:14:57 +0500

    Задание 2 урока 19

---
+++ Task_19_02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_19_02
+{
+    internal class Program
+    {
+        /*2.Напишите программу, которая запрашивает у пользователя произвольный текст,
+         * содержащий предложения (есть знаки препинания). программу после анализа выводит текст, 
+         * разделенный на части:
+        a)По пробелам (отдельные слова построчно)
+        b)По предложениям (отдельные предложения построчно)
+        (используйте метод Split())
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            string[] textArray = text.Split();
+
+            foreach (string str in textArray)
+                Console.WriteLine(str);
+
+            Console.WriteLine("\n");
+
+            string[] textArray2 = text.Split('.');
+
+            foreach (string str2 in textArray2)
+                Console.WriteLine(str2);
+        }
+    }
+}
