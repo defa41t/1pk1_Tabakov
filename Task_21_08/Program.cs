f0165bc4d5d7120a9261d0a520d8eaf4c560a54f
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-03-30 19:10:45 +0500

    Задание 8 урока 21

---
+++ Task_21_08/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_21_08
+{
+    internal class Program
+    {
+        /*напишите метод, который на вход получается массив параметров (строк) и возвращает только уникальные строки */
+        static void Main(string[] args)
+        {
+            string[] strings = { "hello", "world", "programming", "C#", "world", "C#" };
+            string[] uniqueStrings = GetStrings(strings);
+
+            Console.WriteLine("Массив строк до: ");
+            foreach (string s in strings) 
+                Console.Write(s + ", ");
+            Console.WriteLine();
+            Console.WriteLine("Массив строк после: ");
+            foreach(string i in uniqueStrings)
+                Console.Write(i + ", ");
+            
+            
+        }
+        static string[] GetStrings(string[] strings)
+        {
+            HashSet<string> uniqueSet = new HashSet<string>(strings);
+            return new string[uniqueSet.Count];
+        }
+    }
+}
