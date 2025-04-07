145fba82422e6ed675b7317d3c907c1cecf28fb1
Иван Табаков <ivan.tabakov.10@gmail.com>
2025-04-07 23:23:32 +0500

    Задание 8 урока 24

---
+++ Task_24_08/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_24_08
+{
+    internal class Program
+    {
+        /*реализуйте функцию, осуществляющую поиск текста в файле 
+         * и его замену на значения, введенные пользователем
+         */
+        static void Main(string[] args)
+        {
+            string path = "text.txt";
+
+            Console.Write("Введите текст для поиска: ");
+            string searchText = Console.ReadLine();
+
+            Console.Write("Введите текст для замены: ");
+            string replacementText = Console.ReadLine();
+
+            // Чтение содержимого
+            string fileContent = File.ReadAllText(path);
+
+            // Выполнение замены
+            string updatedContent = fileContent.Replace(searchText, replacementText);
+
+            // Сохранение изменении
+            File.WriteAllText(path, updatedContent);
+        }
+    }
+}
